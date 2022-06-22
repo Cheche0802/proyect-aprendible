ec9cef945deb69332138aa975af83fe529fa29da
Cheche0802 <jmonterrey1993@gmail.com>
2022-06-21 21:21:45 -0400

    Creacion de plantilla

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>contact</title>
-</head>
-<body>
-<h1>contacto</h1>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title', 'Contacto')
+
+@section('content')
+<h1>Contacto</h1>
+@endsection
+
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
-</head>
-<body>
-@include('partials.navigation')
+@extends('layouts.app')
+
+@section('title', 'Home')
+@section('meta-descripcion', 'descripcion del home')
+
+
+@section('content')
 <h1>Inicio</h1>
-</body>
-</html>
+@endsection
+
+
