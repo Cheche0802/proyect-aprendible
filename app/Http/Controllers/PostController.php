d7abc6578ca11e3380b69525f90e43fac1429b30
Jose Monterrey <jmonterrey1993@gmail.com>
2022-06-29 16:30:49 -0400

    Conexion a Bd leccion 11

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,18 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
-       public function methodprueba()
+       public function index()
     {
-        $posts =[
-            ['title'=> 'First Post'],
-            ['title'=> 'Second Post'],
-            ['title'=> 'Third Post'],
-            ['title'=> 'Fourth Post'],
-            ['title'=> 'Fiveth Post'],
-          ];
+        $posts =DB::table('posts')->get();
 
       return view('blog', ['posts' => $posts]);
     }
--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -6,7 +6,7 @@
     <h1>Blog</h1>
 
     @foreach ($posts as $post)
-      <h3>   {{ $post['title']  }} </h3>
+      <h3>   {{ $post->title  }} </h3>
     @endforeach
 
 </x-layouts.app>
