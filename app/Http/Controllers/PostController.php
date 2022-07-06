f5105fd0033f0cd9529078243e00a44466479138
Jose Monterrey <jmonterrey1993@gmail.com>
2022-07-06 09:27:29 -0400

    Clase 18 mensajes de sesion

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -28,6 +28,10 @@ class PostController extends Controller
     }
 
     public function store(Request $request){
+        $request->validate([
+            'title' => ['required'],
+            'body' => ['required'],
+        ]);
         $post = new Post;
 
          $post->title = $request->input('title');
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,22 +7,34 @@ meta-description="Este es un post nuevo">
 
 <h1> Create New post</h1>
 
+@dump($errors->all())
 <form action="{{route('posts.store')}}" method="POST">
     @csrf
     <label for="title">
         Title <br>
-        <input type="text" name="title" id="title">
-    </label><br>
+        <input type="text" name="title" id="title" required value="{{old('title')}}">
+        <br>
+        @error('title')
+            <small style="color: red">{{$message}}</small>
+            <br>
+        @enderror
+    </label>
 
     <label for="body">
         Body<br>
-        <textarea name="body" id="body" cols="30" rows="10"></textarea>
+        <textarea name="body" id="body" cols="30" rows="10" required >{{old('body')}}</textarea>
     </label><br>
+    <br>
+    @error('body')
+    <small style="color: red">{{$message}}</small>
+    <br>
+    @enderror
+
     <button type="submit">Enviar </button><br>
 </form><br>
 
 
-<a href="{{route('posts.index')}}">Regresar</a>
+<a href="{{route('posts.index')}}">Regresar</a>l
 
 
 </x-layouts.app>
